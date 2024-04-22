4e9a0b205c6c3653ceb1894fe12d6880e4801bd0
tulusdeveloper <tulusdeveloper@gmail.com>
2024-04-22 08:53:16 +0300

    rebuild

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import Footer from "../Footer";
-import Features from "../Features";
 
 const ECommerce: React.FC = () => {
   return (
@@ -14,7 +13,7 @@ const ECommerce: React.FC = () => {
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
        
       </div>
-      <Features/>
+      
       <Footer/>
     </>
   );
