fb6ab0f85ce3472fecc88e4f54f08eb58addb561
tulusdeveloper <tulusdeveloper@gmail.com>
2024-05-15 12:29:46 +0300

    slider

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,16 +1,12 @@
-// carousel.tsx
 "use client"
+
 import Image from 'next/image'
 import React, { useEffect, useRef, useState } from 'react'
 
-const Carousel = ({ data }: {
-    data: {
-        image: string
-    }[]
-}) => {
+const Carousel = ({ data }: { data: { image: string }[] }) => {
     // State and Ref initialization
     const [currentImg, setCurrentImg] = useState(0)
-    const [carouselSize, setCarouselSize] = useState({ width: 0, height: 0 })
+    const [carouselSize, setCarouselSize] = useState({ width: 6720, height: 4480 })
     const carouselRef = useRef(null)
 
     // useEffect to get the initial carousel size
@@ -28,14 +24,15 @@ const Carousel = ({ data }: {
     return (
         <div>
             {/* Carousel container */}
-            <div className='w-80 h-60 rounded-md overflow-hidden relative'>
+            <div className='w-400 h-100 rounded-md overflow-hidden relative'>
                 {/* Image container */}
                 <div
                     ref={carouselRef}
                     style={{
                         left: -currentImg * carouselSize.width
                     }}
-                    className='w-full h-full absolute flex transition-all duration-300'>
+                    className='w-full h-full absolute flex transition-all duration-300'
+                >
                     {/* Map through data to render images */}
                     {data.map((v, i) => (
                         <div key={i} className='relative shrink-0 w-full h-full'>
@@ -43,13 +40,12 @@ const Carousel = ({ data }: {
                                 className='pointer-events-none'
                                 alt={`carousel-image-${i}`}
                                 fill
-                                src={v.image || "https://random.imagecdn.app/500/500"}
+                                src={`/images/slider/${v.image}`}
                             />
                         </div>
                     ))}
                 </div>
             </div>
-
             {/* Navigation buttons */}
             <div className='flex justify-center mt-3'>
                 <button
--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -5,13 +5,21 @@ import Footer from "../Footer";
 import Carousel from "../Carousel";
 
 const ECommerce: React.FC = () => {
+  const data = [
+    { image: 'slider1.jpg' },
+    { image: 'slider2.jpg' },
+    { image: 'slider3.jpg' },
+    { image: 'slider4.jpg' },
+    { image: 'slider5.jpg' },
+    { image: 'slider6.jpg' },
+  ];
+
   return (
     <>
       {/* Render the HomeSlider component */}
       <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-     
       </div>
-      <Carousel data={[]}/>
+      <Carousel data={data} />
       <Footer />
     </>
   );
