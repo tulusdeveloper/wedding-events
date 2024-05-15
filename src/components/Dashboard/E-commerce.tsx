d835a4c70e4b5bf51cd4dae96e5c2ce6b15f6323
tulusdeveloper <tulusdeveloper@gmail.com>
2024-05-15 11:23:11 +0300

    changes

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -1,34 +1,18 @@
 "use client";
+
 import React from "react";
 import Footer from "../Footer";
-import Image from "next/image";
+import Carousel from "../Carousel";
 
 const ECommerce: React.FC = () => {
   return (
     <>
-      <div className="overflow-hidden rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-          <div className="relative z-20 h-35 md:h-65">
-            <Image
-              src={"/images/cover/cover-01.png"}
-              alt="profile cover"
-              className="h-full w-full rounded-tl-sm rounded-tr-sm object-cover object-center"
-              width={970}
-              height={260}
-              style={{
-                width: "auto",
-                height: "auto",
-              }}
-            />
-          
-          </div>
-         
-        </div>
-
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-       
+      {/* Render the HomeSlider component */}
+      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+     
       </div>
-      
-      <Footer/>
+      <Carousel data={[]}/>
+      <Footer />
     </>
   );
 };
